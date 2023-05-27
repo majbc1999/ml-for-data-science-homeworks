819e2ba341d76fff6b1e6f00ef42e6bc77be535e
majbc1999 <majbc1999@gmail.com>
2023-05-27 23:57:00 +0200

    Implementation of Bayesian with MCMC

--- homework-06/hw_bayesian.py
+++ homework-06/hw_bayesian.py
@@ -13,6 +13,18 @@ class BayesianLogisticRegression:
     # Thank the lord for this article
     # https://towardsdatascience.com/introduction-to-bayesian-logistic-regression-7e39a0bae691#:~:text=Bayesian%20logistic%20regression%20has%20the,of%20contraceptives%20usage%20per%20district.
 
+    def __init__(self, n_samples: int = 1000, n_burnin: int = 1000, n_chains: int = 4):
+        self.n_samples = n_samples
+        self.n_burnin = n_burnin
+        self.n_chains = n_chains
+        self.model = None
+        self.trace = None
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        """
+        Fit the model to the data.
+        """
+        
 
 # misc
 
